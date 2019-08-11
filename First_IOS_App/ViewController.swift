2273f11bec2f13c26ba17375265ad7aacd131aba
Alexstn27 <alexuzu_mes@yahoo.com>
2019-08-11 10:56:25 +0300

    Initial Commit

---
+++ First_IOS_App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  First_IOS_App
+//
+//  Created by STAN ALEXANDRU on 8/11/19.
+//  Copyright © 2019 STAN ALEXANDRU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
